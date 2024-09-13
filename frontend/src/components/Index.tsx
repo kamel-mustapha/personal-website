4a9f984b4f010175a3950e5cea3c7be760cc8e59
musk96 <musk96@hotmail.fr>
2024-09-13 14:40:51 +0200

    added favicon

--- frontend/src/components/Index.tsx
+++ frontend/src/components/Index.tsx
@@ -2,7 +2,7 @@ export function Index() {
   return (
     <>
       <h3 className="mt-14 lg:mt-0 text-4xl">I'm a Software Engineer.</h3>
-      <h4 className="mt-4 lg:mt-0">Currently, I'm a Software Engineer at </h4>
+      <h4 className="mt-4 lg:mt-0">Currently, working at *****</h4>
       <p className="mt-12 max-w-[750px]">
         A self-taught software engineer, working in the industry for 4+ years. I build efficient and scalable digital solutions that balance user requirements with business objectives.
       </p>
