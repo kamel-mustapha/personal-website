ea161afa723bf698d0d34937416b78ad380b4a07
musk96 <musk96@hotmail.fr>
2024-12-04 15:39:19 +0100

    added translations in portfolio page

--- frontend/src/components/portfolio/Projects.tsx
+++ frontend/src/components/portfolio/Projects.tsx
@@ -6,56 +6,108 @@ import Statuschecks from "../../assets/statuschecks-home.png";
 import Easygestion from "../../assets/gestion-home.jpg";
 import Lamacta from "../../assets/lamacta-home.png";
 import { Presentation } from "./Presentation";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { LangContext } from "../../contexts/LangContext";
+
+interface ProjectType {
+  name: string;
+  preview?: string;
+  image: string;
+  position: {
+    en: string;
+    fr: string;
+  };
+  description: {
+    en: string;
+    fr: string;
+  };
+}
 
 export function Projects() {
+  const language = useContext(LangContext);
   const [dialog, setDialog] = useState(false);
-  const projects = [
+  const projects: ProjectType[] = [
     {
       name: "Minelead",
       preview: "https://minelead.io",
       image: Minelead,
-      position: "Full Stack Developer",
-      description:
-        "Find Emails and Generate Leads. Cutting Edge Email Finder and Verifier, with Email scrapper for LinkedIn and more features for the best lead generation tool. find email for any company or website as you visit them.",
+      position: {
+        en: "Full Stack Developer",
+        fr: "Développeur Full Stack",
+      },
+      description: {
+        en: "Find Emails and Generate Leads. Cutting Edge Email Finder and Verifier, with Email scrapper for LinkedIn and more features for the best lead generation tool. find email for any company or website as you visit them.",
+        fr: `Trouvez des e-mails et générez des leads. Un outil de pointe pour trouver et vérifier des e-mails, avec un extracteur d'e-mails pour LinkedIn et bien plus de fonctionnalités pour le meilleur outil de génération de leads. Trouvez des adresses e-mail pour toute entreprise ou site web au fur et à mesure que vous les visitez.`,
+      },
     },
     {
       name: "Wissal Academy",
       preview: "https://wissalacademy.com",
       image: Wissal,
-      position: "Full Stack Developer / Frontend Tech Lead",
-      description:
-        "Wissal Academy is a web platform designed for managing training schools. It offers a showcase website along with a back-office system, enabling users to efficiently manage training programs, students, instructors, and certifications. The platform provides an integrated solution for streamlining operations and enhancing the visibility of educational institutions.",
+      position: {
+        en: "Full Stack Developer / Tech Lead",
+        fr: "Développeur Full Stack / Lead Tech",
+      },
+      description: {
+        en: "Wissal Academy is a web platform designed for managing training schools. It offers a showcase website along with a back-office system, enabling users to efficiently manage training programs, students, instructors, and certifications. The platform provides an integrated solution for streamlining operations and enhancing the visibility of educational institutions.",
+        fr: `Wissal Academy est une plateforme web conçue pour la gestion des écoles de formation. Elle propose un site vitrine ainsi qu’un système de back-office, permettant aux utilisateurs de gérer efficacement les programmes de formation, les étudiants, les instructeurs et les certifications. La plateforme offre une solution intégrée pour optimiser les opérations et améliorer la visibilité des établissements éducatifs.`,
+      },
     },
     {
       name: "Carto",
       image: Carto,
-      position: "Tech Lead",
-      description:
-        "Carto is an innovative web application designed to simplify the design, visualization, and management of complex application architectures. Whether you’re organizing open or restricted data flows, Carto provides a powerful, intuitive interface that enhances clarity and productivity.",
+      position: {
+        en: "Tech Lead",
+        fr: "Lead Tech",
+      },
+      description: {
+        en: "Carto is an innovative web application designed to simplify the design, visualization, and management of complex application architectures. Whether you’re organizing open or restricted data flows, Carto provides a powerful, intuitive interface that enhances clarity and productivity.",
+        fr: `Carto est une application web innovante conçue pour simplifier la conception, la visualisation et la gestion des architectures d'applications complexes. Que vous organisiez des flux de données ouverts ou restreints, Carto offre une interface puissante et intuitive qui améliore la clarté et la productivité.`,
+      },
     },
     {
       name: "Statuscheks",
       image: Statuschecks,
-      position: "Founder / Full Stack Developer",
-      description:
-        "StatusChecks is your all-in-one solution for monitoring websites, servers, APIs, and databases. Designed to keep your services running smoothly, it offers real-time monitoring, instant alerts, and detailed reports, so you can address potential issues before they impact your business.",
+      position: {
+        en: "Founder / Full Stack Developer",
+        fr: "Fondateur / Développeur full stack",
+      },
+      description: {
+        en: "StatusChecks is your all-in-one solution for monitoring websites, servers, APIs, and databases. Designed to keep your services running smoothly, it offers real-time monitoring, instant alerts, and detailed reports, so you can address potential issues before they impact your business.",
+        fr: `StatusChecks est votre solution tout-en-un pour surveiller les sites web, serveurs, APIs et bases de données. Conçu pour garantir le bon fonctionnement de vos services, il offre une surveillance en temps réel, des alertes instantanées et des rapports détaillés, vous permettant de résoudre les problèmes potentiels avant qu'ils n'affectent votre activité.`,
+      },
     },
     {
       name: "Easygestion",
       image: Easygestion,
-      position: "Founder / Full Stack Developer",
-      description:
-        "EasyGestion is a desktop software tailored for merchants, including school cafeterias, to streamline their business operations. It offers essential tools for managing inventory, generating invoices and delivery notes, and handling sales, providing a simple and efficient solution for daily commerce management.",
+      position: {
+        en: "Founder / Full Stack Developer",
+        fr: "Fondateur / Développeur full stack",
+      },
+      description: {
+        en: "EasyGestion is a desktop software tailored for merchants, including school cafeterias, to streamline their business operations. It offers essential tools for managing inventory, generating invoices and delivery notes, and handling sales, providing a simple and efficient solution for daily commerce management.",
+        fr: `EasyGestion est un logiciel de bureau conçu pour les commerçants, y compris les cantines scolaires, afin de simplifier la gestion de leurs opérations commerciales. Il propose des outils essentiels pour gérer les stocks, générer des factures et des bons de livraison, ainsi que gérer les ventes, offrant une solution simple et efficace pour la gestion quotidienne du commerce.`,
+      },
     },
     {
       name: "Lamacta",
       image: Lamacta,
-      position: "Full Stack Developer",
-      description:
-        "Lamacta is your go-to platform for buying, selling, and renting real estate in Algeria. Whether you're searching for apartments, villas, or commercial properties, Lamacta simplifies the process with thousands of listings, a user-friendly interface, and a property price estimation tool to help you make informed decisions.",
+      position: {
+        en: "Full Stack Developer",
+        fr: "Développeur full stack",
+      },
+      description: {
+        en: "Lamacta is your go-to platform for buying, selling, and renting real estate in Algeria. Whether you're searching for apartments, villas, or commercial properties, Lamacta simplifies the process with thousands of listings, a user-friendly interface, and a property price estimation tool to help you make informed decisions.",
+        fr: `Lamacta est votre plateforme de référence pour l'achat, la vente et la location de biens immobiliers en Algérie. Que vous recherchiez des appartements, des villas ou des propriétés commerciales, Lamacta simplifie le processus avec des milliers d'annonces, une interface conviviale et un outil d'estimation des prix des biens immobiliers pour vous aider à prendre des décisions éclairées.`,
+      },
     },
   ];
+  const TRANSLATIONS = {
+    PREVIEW: {
+      en: "PREVIEW",
+      fr: "APERÇU",
+    },
+  };
 
   // const showDialog = () => {
   //   setDialog(true);
@@ -71,17 +123,17 @@ export function Projects() {
         {projects.map((project, index) => (
           <div key={index} className={`project-case animate__animated animate__fadeIn ${index % 2 > 0 ? "lg:flex-row-reverse" : "lg:flex-row "}`}>
             <div className="description">
-              <h4 className={`${index % 2 > 0 ? "lg:text-right" : ""}`}>{project.position}</h4>
+              <h4 className={`${index % 2 > 0 ? "lg:text-right" : ""}`}>{project.position[language!.state]}</h4>
               <h3 className={`${index % 2 > 0 ? "lg:text-right" : ""}`}>{project.name}</h3>
-              <p className={`text ${index % 2 > 0 ? "lg:-translate-x-28" : ""}`}>{project.description}</p>
-              <p className={`filler ${index % 2 > 0 ? "lg:-translate-x-28" : ""}`}>{project.description}</p>
+              <p className={`text ${index % 2 > 0 ? "lg:-translate-x-28" : ""}`}>{project.description[language!.state]}</p>
+              <p className={`filler ${index % 2 > 0 ? "lg:-translate-x-28" : ""}`}>{project.description[language!.state]}</p>
               <div className={`${index % 2 > 0 ? "lg:justify-end" : ""}`}>
                 {/* <button className="button-primary" onClick={showDialog}>
                   LEARN MORE
                 </button> */}
                 {project.preview && (
                   <a className="button-secondary" href={project.preview} target="_blank">
-                    Preview
+                    {TRANSLATIONS.PREVIEW[language!.state]}
                   </a>
                 )}
               </div>
--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -1,18 +1,35 @@
+import { useContext } from "react";
 import { Projects } from "../components/portfolio/Projects";
+import { LangContext } from "../contexts/LangContext";
 
 export function Portfolio() {
+  const language = useContext(LangContext);
+  const TRANSLATIONS = {
+    TITLE: {
+      en: `Throughout my career, I've had the opportunity to work on a wide variety of projects across different domains, including e-commerce, e-learning, marketing, and desktop software. I've worn
+        multiple hats, such as backend and frontend developer, and have also served as a tech lead on several occasions, where I led teams of developers.`,
+      fr: `Au cours de ma carrière, j'ai eu l'opportunité de travailler sur une grande variété de projets dans différents domaines, notamment le e-commerce, l'e-learning, 
+      le marketing et les logiciels de bureau. J'ai occupé plusieurs rôles, tels que développeur backend et frontend, et j'ai également exercé en tant que leader technique 
+      à plusieurs reprises, où j'ai dirigé des équipes de développeurs.`,
+    },
+    ADDITION: {
+      en: `In addition to these projects, I have also contributed as a consultant on several short-term assignments, providing tailored solutions and delivering immediate impact.`,
+      fr: `En plus de ces projets, j'ai également contribué en tant que consultant sur plusieurs missions de courte durée, en proposant des solutions sur mesure et en apportant un impact immédiat.`,
+    },
+    CONTACT: {
+      en: "CONTACT ME",
+      fr: "ME CONTACTER",
+    },
+  };
   return (
     <>
       <h1 className="mt-16 text-4xl">Portfoio</h1>
-      <p className="mt-4 max-w-[900px] bg-transparent leading-relaxed">
-        Throughout my career, I've had the opportunity to work on a wide variety of projects across different domains, including e-commerce, e-learning, marketing, and desktop software. I've worn
-        multiple hats, such as backend and frontend developer, and have also served as a tech lead on several occasions, where I led teams of developers.
-      </p>
+      <p className="mt-4 max-w-[900px] bg-transparent leading-relaxed">{TRANSLATIONS.TITLE[language!.state]}</p>
       <Projects />
       <p className="text-center mt-60">
-        <div>In addition to these projects, I have also contributed as a consultant on several short-term assignments, providing tailored solutions and delivering immediate impact.</div>
+        <div>{TRANSLATIONS.ADDITION[language!.state]}</div>
         <a className="!inline-block mt-8 button-primary" href="mailto:kamel.dine@hotmail.com">
-          CONTACT ME
+          {TRANSLATIONS.CONTACT[language!.state]}
         </a>
       </p>
     </>
